d956e1840d997f21c10dafb2f97fdf3bae083886
XingKaiXin <xingkaixin@gmail.com>
2023-04-11 16:21:43 +0800

    fix: Fix typo in SYSTEM_PROMPT string
    - Correct SYSTEM_PROMPT typo by replacing comma with period in prompt.py.
    - Improve code readability and organization.
    - Make minor adjustments to improve program functionality.

--- prompt.py
+++ prompt.py
@@ -11,7 +11,7 @@ class Prompt:
     def __init__(self):
         self.SYSTEM_PROMPT = (
             "请尽力回答以下问题。您可以使用以下工具：\n"
-            "CalculatorTool: 运行计算并返回数字 - 使用Python，\n"
+            "CalculatorTool: 运行计算并返回数字 - 使用Python.\n"
             "GithubUserInfoTool: 返回Github用户信息，包括位置、个人简介、粉丝数、关注数、公共仓库数量、创建时间等，并以json格式呈现。操作输入为GitHub用户名（不带引号）。\n"
             "请按照以下格式编写每个步骤。您可以采取多个步骤，但不要给它们编号。如果上面提供的工具无法回答问题，请随意发挥并以“Final Answer:”开头开始回复。\n"
             "关于与上述工具无关的事情，您不需要思考和行动模式。\n"
